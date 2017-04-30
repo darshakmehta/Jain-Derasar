6e010deefae4496ceb858770e03bbc4002a5a1f7
Darshak Mehta <Darshak Mehta>
2017-04-30 12:30:22 +0530

    resourceurl fixed

--- app/services/derasarService.js
+++ app/services/derasarService.js
@@ -1,7 +1,7 @@
 //This file contains REST APIs calls
 var $ = require("jquery");
 var promise = require("es6-promise");
-var resourceUrl = process.env.MONGOLAB_URI + "api/derasars";
+var resourceUrl = "https://jain-derasar.herokuapp.com/api/derasars";
 
 module.exports = {
     addDerasar: function (derasar) {
